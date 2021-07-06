53775d1554e54e8cfe2a1c75f0614cf96751336c
Arjun Muraleedharan <arjunmcd0321@gmail.com>
2021-07-07 02:08:35 +0530

    Revamped personal website (4)

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.style.width = "70%";
+}
+
+function closeSidebar() {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.style.width = "0";
+}
