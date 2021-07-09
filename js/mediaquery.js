def9bac5a20618692e40cf96388e95ee04ff2b50
Arjun Muraleedharan <arjunmcd0321@gmail.com>
2021-07-10 04:18:15 +0530

    Made minor changes

--- js/mediaquery.js
+++ js/mediaquery.js
@@ -1,11 +1,13 @@
 function changeFeatures(x) {
     if (x.matches) {
         $('nav').html("<div onclick='openSidebar()' id='hamburger-icon'>&#9776;</div>");
+        $('#mes3').html("Press anywhere to find out more about me!")
     }
     else {
         $('nav').html(
             '<a href="#welcome"><i class="fa fa-home" aria-hidden="true"></i></a><a href="#">About</a><a href="#">Skills</a><a href="#">Projects</a><a href="#">Contact</a><a href="#">Social Media</a>'
         );
+        $('#mes3').html("Click anywhere to find out more about me!")
     }
 }
 
