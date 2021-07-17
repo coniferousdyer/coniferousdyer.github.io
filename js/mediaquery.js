5e879f3efea52fb87279c13264784d4d88a7460c
Arjun Muraleedharan <arjunmcd0321@gmail.com>
2021-07-18 04:18:43 +0530

    Changed font of navbar

--- js/mediaquery.js
+++ js/mediaquery.js
@@ -4,7 +4,7 @@ function changeFeatures(x) {
     }
     else {
         $('nav').html(
-            '<a href="#"><i class="fa fa-home" aria-hidden="true"></i></a><a href="#">About</a><a href="#">Skills</a><a href="#">Projects</a><a href="#">Contact</a><a href="#">Social Media</a>'
+            '<a href="#"><i class="fa fa-home" aria-hidden="true"></i></a><a href="#">ABOUT</a><a href="#">SKILLS</a><a href="#">PROJECTS</a><a href="#">CONTACT</a><a href="#">SOCIAL MEDIA</a>'
         );
     }
 }
