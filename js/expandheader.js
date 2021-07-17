7e6c6bb63d2112cdd1894e8a94ba5b690ca2fc2a
Arjun Muraleedharan <arjunmcd0321@gmail.com>
2021-07-18 04:49:03 +0530

    Made minor change

--- js/expandheader.js
+++ js/expandheader.js
@@ -1,6 +1,8 @@
-setTimeout(function expandHeader() {
-    document.getElementById("header").style.width = "48%";
+function expandHeader() {
     setTimeout(() => {
-        $('nav').css("opacity", "1")
-    }, 2000);
-}, 500);
+        document.getElementById("header").style.width = "48%";
+        setTimeout(() => {
+            $('nav').css("opacity", "1")
+        }, 2000);
+    }, 500);
+}
