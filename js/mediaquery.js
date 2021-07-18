5940148c0e842bb5d7ed003cee479abf90a315c2
Arjun Muraleedharan <arjunmcd0321@gmail.com>
2021-07-18 21:56:05 +0530

    Added files

--- js/mediaquery.js
+++ js/mediaquery.js
@@ -4,7 +4,7 @@ function changeFeatures(x) {
     }
     else {
         $('nav').html(
-            '<a href="#"><i class="fa fa-home" aria-hidden="true"></i></a><a href="#">ABOUT</a><a href="#">SKILLS</a><a href="#">PROJECTS</a><a href="#">CONTACT</a><a href="#">SOCIAL MEDIA</a>'
+            '<a href="#"><i class="fa fa-home" aria-hidden="true"></i></a><a href="#">ABOUT</a><a href="#">SKILLS</a><a href="#">PROJECTS</a><a href="#">CONTACT</a>'
         );
     }
 }
