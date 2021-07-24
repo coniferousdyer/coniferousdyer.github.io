0813fb8d0e3fcf86223fa79fd100b274a7abf49b
Arjun Muraleedharan <arjunmcd0321@gmail.com>
2021-07-24 20:45:59 +0530

    Revamped personal website(6)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-tsParticles.load("tsparticles", {
+tsParticles.load("home", {
     "fps_limit": 60,
     "interactivity": {
         "detectsOn": "canvas",
@@ -15,57 +15,9 @@ tsParticles.load("tsparticles", {
         }
     },
     "particles": {
-        "color": { "value": "#ffffff" },
+        "color": { "value": "#c5c6c7" },
         "links": {
-            "color": "#ffffff",
-            "distance": 150,
-            "enable": true,
-            "opacity": 0.2,
-            "width": 1
-        },
-        "move": {
-            "bounce": false,
-            "direction": "none",
-            "enable": true,
-            "outMode": "out",
-            "random": false,
-            "speed": 1,
-            "straight": false
-        },
-        "number": {
-            "density": { "enable": true, "area": 800 },
-            "value": 100
-        },
-        "opacity": { "value": 0.5 },
-        "shape": { "type": "circle" },
-        "size": {
-            "random": true,
-            "value": 2
-        }
-    },
-    "detectRetina": true
-});
-
-tsParticles.load("projects-header", {
-    "fps_limit": 60,
-    "interactivity": {
-        "detectsOn": "canvas",
-        "events": {
-            "onClick": { "enable": true, "mode": "push" },
-            "onHover": {
-                "enable": true, "mode": "repulse"
-            },
-            "resize": true
-        },
-        "modes": {
-            "push": { "particles_nb": 4 },
-            "repulse": { "distance": 100, "duration": 0.4 }
-        }
-    },
-    "particles": {
-        "color": { "value": "#ffffff" },
-        "links": {
-            "color": "#ffffff",
+            "color": "#c5c6c7",
             "distance": 150,
             "enable": true,
             "opacity": 0.2,
